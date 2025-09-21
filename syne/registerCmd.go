d00066af64749027da83410be122fe4c645cd193
alanlu <alanlu2468@gmail.com>
2025-09-22 03:17:39 +1000

    :sparkles: syne register command working

--- syne/registerCmd.go
+++ syne/registerCmd.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/yarlson/pin"
 )
 
 type RegisterCmd struct {
@@ -12,16 +18,44 @@ type RegisterCmd struct {
 }
 
 func (c *RegisterCmd) Run() error {
-	if c.Force && c.Name == "" {
-		return fmt.Errorf("Register requires name when using force.")
+	if c.Force {
+		if c.Name == "" {
+			return fmt.Errorf("Register requires name when using force.")
+		}
+
+		Reg[c.Name] = &Server{Url: c.Url}
+		return Reg.Write()
 	}
 
 	if c.Name == "" {
 		c.Name = "Get from server"
 	}
 
-	fmt.Printf("Adding %s %s\n", Cli.Register.Name, Cli.Register.Url)
-	Reg[Cli.Register.Name] = &Server{Url: Cli.Register.Url}
+	c.Name = strings.TrimSpace(c.Name)
+
+	path := fmt.Sprintf("http://%s/name", c.Url)
+
+	p := pin.New("Getting Server Name...")
+	cancel := p.Start(context.Background())
+	defer cancel()
+
+	name, err := http.Get(path)
+
+	if err != nil {
+		return err
+	}
+	if name.StatusCode != 200 {
+		return fmt.Errorf("Server error")
+	}
+	name_bytes, err := io.ReadAll(name.Body)
+	if err != nil {
+		return err
+	}
+	c.Name = string(name_bytes)
+
+	fmt.Printf("Adding %s %s\n", c.Name, c.Url)
+	Reg[c.Name] = &Server{Url: c.Url}
+	p.Stop("Done!")
 
 	return Reg.Write()
 }
